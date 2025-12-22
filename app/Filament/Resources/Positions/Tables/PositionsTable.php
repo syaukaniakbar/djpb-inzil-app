563a98256704145a698bf8a22cbe156311b05144
syaukaniakbar <syaukaniakbar2019@gmail.com>
2025-12-22 16:35:57 +0800

    update position resources

---
+++ app/Filament/Resources/Positions/Tables/PositionsTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Filament\Resources\Positions\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class PositionsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('code')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateHeading('Tidak Ada Data Jabatan')
+            ->emptyStateDescription('Klik tombol "Tambah Jabatan" untuk membuat data baru.');
+    }
+}
